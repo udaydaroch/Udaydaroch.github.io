8e80a5414f00e8610f9900b9c8a3633ab54b1124
Uday <Uda12@uclive.ac.nz>
2024-01-18 14:05:16 +1300

    commit

--- index.js
+++ index.js
@@ -2,24 +2,6 @@ const cricketBall = document.getElementById('ball');
 let currentImageIndex = 2;
 
 cricketBall.addEventListener('animationiteration', () => {
-  currentImageIndex = (currentImageIndex % 5) + 2; // Loop through 1 to 5
-  cricketBall.src = `CRICKETBALL/cricket (${currentImageIndex}).png`; // Change to the next image
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Add an event listener to the "About Me" link
-  document.querySelector('.me').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-
-    // Get the target element's offset from the top of the page
-    const targetElement = document.getElementById('me');
-    const targetOffset = targetElement.offsetTop - 100;
-
-    // Scroll to the target element with a smooth animation
-    window.scrollTo({
-      top: targetOffset,
-      behavior: 'smooth'
-    });
-  });
+  currentImageIndex = (currentImageIndex % 5) + 2; 
+  cricketBall.src = `CRICKETBALL/cricket (${currentImageIndex}).png`; 
 });
